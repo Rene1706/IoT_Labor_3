62b523533ca0b0535d259c22a6c1c8d7f7cf3141
Rene1706 <rene.huertgen@gmx.de>
2023-06-04 21:18:33 +0200

    Added raspivid/mediaplayer and arg parse for tcp/udp and cmd

--- client.py
+++ client.py
@@ -1,8 +1,21 @@
 import socket
+import subprocess
+import argparse
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Video Streaming')
+parser.add_argument('--tcp', action='store_true', help='Use TCP protocol')
+parser.add_argument('--udp', action='store_true', help='Use UDP protocol')
+parser.add_argument('--cmd', type=str, default="raspivid -t 0 -fps 20 -w 1280 -h 720 -b 2000000 -o -", help='Command for raspivid')
+args = parser.parse_args()
 
 # IP und Port des Servers
 IP = '172.23.x.x'
-PORT = yyyy
+PORT = 5000
+
+# raspivid cmd command
+cmd_raspivid = args.cmd
+rasprocess = subprocess.Popen(cmd_raspivid, shell=True, stdout=subprocess.PIPE)
 
 # Lesepuffergroesse
 BUFFER_SIZE = 1400
@@ -15,20 +28,28 @@ socket_types = (socket.SOCK_STREAM, socket.SOCK_DGRAM, socket.SOCK_RAW)
 
 # Passenden Address- und Sockettyp waehlen
 address_family = address_families[0]
-socket_type = socket_types[0]
+if args.tcp:
+    socket_type = socket_types[0]
+elif args.udp:
+    socket_type = socket_types[1]
+else:
+    raise ValueError("Please specify either --tcp or --udp")
 
 # Erstellen eines Sockets (TCP und UDP)
 sock = socket.socket(address_family, socket_type)
 
 # Verbinden zu einem Server-Socket (Nur TCP)
-sock.connect((IP,PORT))
+if args.tcp:
+    sock.connect((IP,PORT))
 
 # Sende immer wieder "Hello" an den Server
 while True:
     message = "Hello"
         
     # TCP
-    sock.send(message)
+    if args.tcp:
+        sock.send(message.encode())
         	
     # UDP
-    sock.sendto(message, (IP, PORT))
+    if args.udp:
+        sock.sendto(message.encode(), (IP, PORT))
--- server.py
+++ server.py
@@ -1,4 +1,17 @@
 import socket
+import subprocess
+import argparse
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Video Server')
+parser.add_argument('--tcp', action='store_true', help='Use TCP protocol')
+parser.add_argument('--udp', action='store_true', help='Use UDP protocol')
+parser.add_argument('--cmd', type=str, default="mplayer -fps 25 -cache 512 -", help='Command for mediaplayer')
+args = parser.parse_args()
+
+# mediaplayer cmd command
+cmd_mplayer = args.cmd
+mprocess = subprocess.Popen(cmd_mplayer, shell=True, stdin=subprocess.PIPE)
 
 # Port des Servers
 PORT = 5000
@@ -14,7 +27,13 @@ socket_types = (socket.SOCK_STREAM, socket.SOCK_DGRAM, socket.SOCK_RAW)
 
 # Passenden Address- und Sockettyp waehlen
 address_family = address_families[0]
-socket_type = socket_types[0]
+if args.tcp:
+	socket_type = socket_types[0]
+elif args.udp:
+	socket_type = socket_types[1]
+else:
+    raise ValueError("Please specify either --tcp or --udp")
+
 
 # Maximale Anzahl der Verbindungen in der Warteschlange
 backlog = 1
@@ -24,16 +43,18 @@ sock = socket.socket(address_family, socket_type)
 sock.bind(('', PORT))
 
 # Lausche am Socket auf eingehende Verbindungen (Nur TCP)
-sock.listen(backlog)
-clientsocket, address  = sock.accept()
+if args.tcp:
+	sock.listen(backlog)
+	clientsocket, address  = sock.accept()
 
 # Daten (der Groesse BUFFER_SIZE) aus dem Socket holen, ausgeben und zuruecksenden:
 while True:
-
     # TCP:
-	data = clientsocket.recv(BUFFER_SIZE)
-	print(data)
+	if args.tcp:
+		data = clientsocket.recv(BUFFER_SIZE)
+		print(data)
 
     # UDP:
-	#data, address = sock.recvfrom(BUFFER_SIZE)
-	#print(data)
+	if args.udp:
+		data, address = sock.recvfrom(BUFFER_SIZE)
+		print(data)
