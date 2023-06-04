c48f18de185c48374d40e49ceb981b3144024a62
Rene1706 <rene.huertgen@gmx.de>
2023-06-04 22:41:55 +0200

    Added shell script to send video 60 sec

--- send_video_60_sec.sh
+++ send_video_60_sec.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 #Run tcpdump to sniff tcp traffic
-sudo tcpdump -n -i wlp4s0 tcp -w server.pcap &
+sudo tcpdump -n -i eth0 tcp -w client.pcap &
 
 # Save the process ID (PID) of the tcpdump process
 pid_tcpdump=$!
 
 # Run the Python script in the background
-python server.py --tcp &
+python /home/pi/IoT/Labor_3/client_1.py --tcp &
 
 # Save the process ID (PID) of the Python script
 pid_python=$!
 
-#ssh pi@192.168.0.55 "/home/pi/IoT/Labor_3/run_video_60_sec.sh"
 # Sleep for 60 seconds
-sleep 20
+sleep 10
 
 # Kill the Python script using the PID
 kill -9 $pid_python
